f2dde35b0e1cf6bb48f5b71f461d447ae100a530
Joey Steele <jsteele@gatech.edu>
2023-03-03 16:50:02 -0500

    Add cursed sound and image changes for echo and cursed

--- quack.js
+++ quack.js
@@ -3,6 +3,10 @@ const QUACK_DELAY_TIME = 0.1; // seconds, for delay effect
 const QUACK_DELAY_PROBABILITY = 0.3; // probability, between 0 and 1
 const QUACK_FDBK = 0.7; // between 0 and 1
 
+const NORMAL_WS_CURVE = new Float32Array([-1, 0, 1]);
+const CURSED_WS_CURVE = new Float32Array([-1, -1, -1, -1, -1, -1, -1, -1, 0, 1, 1, 1, 1, 1, 1, 1, 1]);
+const CURSED_WS_PROBABILITY = 0.2; // probability, between 0 and 1
+
 let active = false;
 let delay_enabled = true;
 
@@ -13,10 +17,12 @@ const active_btn = document.querySelector('#active');
 const rate_slider = document.querySelector('#quack-rate');
 const rate_disp = document.querySelector('#quack-rate-disp');
 const delay_btn = document.querySelector('#delay');
+const duck_pic = document.querySelector('#duck-pic');
 
 let timer;
 let audio_ctx;
 let gain_node;
+let ws_node;
 
 // audio is never active until user interacts with the page
 active_btn.checked = false;
@@ -54,16 +60,31 @@ delay_btn.addEventListener('change', () => {
     }
 });
 
-function play_quack(delay = false) {
+quack.addEventListener('ended', () => { duck_pic.style.filter = 'contrast(100%) blur(0)'; });
+
+function play_quack(delay = false, cursed = false) {
     if (!audio_ctx) {
         build_audio_graph();
     }
 
+    let contrast = '100%';
+    let blur = '0';
+
     if (delay && delay_enabled) {
         gain_node.gain.value = QUACK_FDBK;
+        blur = '10px';
     } else {
         gain_node.gain.value = 0;
     }
+
+    if (cursed) {
+        ws_node.curve = CURSED_WS_CURVE;
+        contrast = '500%';
+    } else {
+        ws_node.curve = NORMAL_WS_CURVE;
+    }
+
+    duck_pic.style.filter = `contrast(${contrast}) blur(${blur})`;
 
     console.log('quack');
 
@@ -90,7 +111,8 @@ quack.addEventListener('ended', schedule_next_quack);
 function schedule_next_quack() {
     const time = time_to_next();
     const delay = Math.random() < QUACK_DELAY_PROBABILITY;
-    timer = setTimeout(() => play_quack(delay), time);
+    const cursed = Math.random() < CURSED_WS_PROBABILITY;
+    timer = setTimeout(() => play_quack(delay, cursed), time);
     console.log(time);
 
     if (time_to_next > 10000) {
@@ -127,15 +149,18 @@ function build_audio_graph() {
     audio_ctx = new AudioContext();
 
     const track = audio_ctx.createMediaElementSource(quack);
+    const ws = new WaveShaperNode(audio_ctx, { curve: NORMAL_WS_CURVE });
     const delay = new DelayNode(audio_ctx, { delayTime: 0.1 });
     const gain = new GainNode(audio_ctx, { gain: 0.7 });
     const add = new GainNode(audio_ctx, { gain: 1 });
 
-    track.connect(add);
+    track.connect(ws);
+    ws.connect(add);
     add.connect(delay);
     add.connect(audio_ctx.destination);
     delay.connect(gain);
     gain.connect(add);
 
     gain_node = gain;
+    ws_node = ws;
 }
